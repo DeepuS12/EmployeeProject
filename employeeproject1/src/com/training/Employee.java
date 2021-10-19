682d9ec4d164f10ec42f966945d6fb0bf4dd9db6
DeepuS12 <deepivs18@gmail.com>
2021-10-19 15:43:07 +0530

    The first commit

---
+++ employeeproject1/src/com/training/Employee.java
@@ -0,0 +1,33 @@
+package com.training;
+
+public class Employee {
+	private int id;
+    private String name;
+    private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String toString() {
+		return "Employee [id=" + id + "]";
+	}
+
+}
